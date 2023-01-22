ab46192dec44bbeebdc32b41e067c1fbb3337d5c
dniero95 <darioniero@icloud.com>
2023-01-22 21:20:30 +0100

    add timestamp microservice

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,20 +21,25 @@ app.get("/", function (req, res) {
 });
 
 
-// // timestamp converter
-// app.get("/api/date", function (req, res) {
-//   let date = req.params.date;
+// timestamp converter
+app.get("/api/:date", function (req, res) {
+  let date = req.params.date
+  // console.log(typeof req.params.date);
+  if (/^\d+$/.test(date)) {
+    date = new Date(+date);
+  } else {
+    date = new Date(date);
+  }
+  if (date.toString() === "Invalid Date") {
+    res.json({ error: date.toString() });
+  } else {
+    res.json({ "unix": date.getTime(), "utc": date.toUTCString() });
+  }
+});
 
-//   console.log(date);
-//   res.json({"unix":date, "utc":date});
-// });
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-// your first API endpoint... 
-app.get("/api/ciao", function (req, res) {
-  res.json({greeting: 'ciao API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
