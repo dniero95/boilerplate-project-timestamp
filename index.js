d3ae2b227bb108f903583f61b297587fcef7118d
dniero95 <darioniero@icloud.com>
2023-01-22 19:40:46 +0100

    add env variables

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var app = express();
 
+require('dotenv').config()
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -19,10 +21,22 @@ app.get("/", function (req, res) {
 });
 
 
+// // timestamp converter
+// app.get("/api/date", function (req, res) {
+//   let date = req.params.date;
+
+//   console.log(date);
+//   res.json({"unix":date, "utc":date});
+// });
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+// your first API endpoint... 
+app.get("/api/ciao", function (req, res) {
+  res.json({greeting: 'ciao API'});
+});
+
 
 
 
